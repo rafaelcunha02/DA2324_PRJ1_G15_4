4223e7ea502f60c2f35ef78bf5cacce5519d6c90
rafaelcunha02 <rafaelscpcunha@gmail.com>
2024-03-05 19:22:45 +0000

    Added data structures and reader class

---
+++ src/reader.h
@@ -0,0 +1,17 @@
+//
+// Created by Rafa on 05/03/2024.
+//
+
+#ifndef DA2324_PRJ1_G15_4_PARSING_H
+#define DA2324_PRJ1_G15_4_PARSING_H
+
+
+class reader {
+private:
+
+
+
+};
+
+
+#endif //DA2324_PRJ1_G15_4_PARSING_H
